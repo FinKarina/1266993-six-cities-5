45bd851bf9b38b289146cef65fc009caf2a37339
Karina Nikonova <karinaaurinkoinen@gmail.com>
2020-10-06 21:32:19 +0300

    Начинаем программировать

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
+import React from "react";
+import ReactDom from "react-dom";
+import App from "./components/app/app";
+
+const Setting = {
+  PLACES_COUNT: 312
+};
+
+ReactDom.render(
+    <App
+      placesCount={Setting.PLACES_COUNT}
+    />,
+    document.querySelector(`#root`)
+);
 
